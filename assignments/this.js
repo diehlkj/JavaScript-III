cfd71977228e0012ddadd2e931f483f11a2b035b
Ken Diehl <diehlkj.99@gmail.com>
2019-10-10 21:25:04 -0400

    principle 2

--- assignments/this.js
+++ assignments/this.js
@@ -2,7 +2,7 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. PRINCIPLE 1: GLOBAL/WINDOW BINDING: this refers to the window object
-* 2. 
+* 2. PRINCIPLE 2: this refers to the value within the object it is called. i.e. Person.sayHi() this will refer to values in Person
 * 3. 
 * 4. 
 *
@@ -22,6 +22,14 @@
 // Principle 2
 
 // code example for Implicit Binding
+
+const Person = {
+    name: 'Ken',
+    workplace: 'BBY',
+    sayHi: function () {
+      console.log(`Hi my name is ${this.name}! I work at ${this.workplace}`)}
+  }
+  Person.sayHi();
 
 // Principle 3
 
